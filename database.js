ea40788d47802570df8f22c35fddceacc8b81fb7
Gizem Telli <gizemtelli8@gmail.com>
2016-01-03 23:08:53 +0200

    :üzgünemoji

---
+++ database.js
@@ -0,0 +1,58 @@
+var catalog =[
+    {
+  "itemName":"Dava",
+        "itemAuthor":"Franz Kafka",
+        "itemImage": "kapak/dava.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":  "<p>Franz Kafka'nın Dava adlı romanının bu çevirisi, yazarın Oxford Metinleri diye adlandırılan el yazıları üzerinde Amerikalı ve Alman uzman-ların yaptıkları son çalışmalarla oluşturulan metinden yapıldı.</p>
+<p>Dava, Korku Çağı diye adlandırılan 20. yüzyılda insanoğlunun artık neredeyse kurtulunması olanaksız bir yazgıya dönüşen kuşatılmış yaşamının öyküsüdür.
+Bu çağa korku egemendir, çünkü insan, hemcinsleriyle insanca bir dil aracılığıyla iletişim kurabilme, böyle bir dille insanca tepkiler uyandırabilme olanağından yoksun kalmıştır. 
+Albert Camus'nün deyişiyle, bu olanağın bulunmadığı bir çağ artık ancak "Korku Çağı" diye adlandırılabilir.</p><p>
+Kafka'nın Dava'da betimlediği yargılama süreci, böyle bir çağın en güçlü simgelerinden biridir ve onun eseri, insan insanın korkusu olarak kaldığı sürece, güncelliğini hiç yitirmeyecektir. </p>"
+},
+    {
+        "itemName":"Fil",
+        "itemAuthor":"Raymond Carver",
+        "itemImage":"kapak/fil.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"<p>Kollarını boynuma dolayıp bana sarılıyor ve başını omzuma yaslıyor. Ama mesele şu: Az önce ona söylediklerim, bütün gün ara ara düşündüklerim, şey, bir tür görünmez çizgiyi aşmışım gibi hissediyorum. Hiç gelmek zorunda kalmayacağımı sandığım bir yere gelmişim gibi hissediyorum. Ve buraya nasıl geldiğimi bilmiyorum. Garip bir yer. Kısa, zararsız bir rüyanın, sonra da sabah erkenden yapılan uykulu bir konuşmanın beni ölüm ve yok oluş düşüncelerine sürüklediği bir yer.</p>
+
+<p>Yaşamın acı yüzüyle bu kadar erken tanışmasaydı, kuşkusuz yine yazar olurdu ama hiçbir zaman okurları tarafından böyle sahiplenilmezdi Raymond Carver. Gençlerin haytalık yapıp havai aşklar kovaladığı yaşlarda o evli ve iki çocuk babasıydı. Hayatı öğrenmenin yolu, bulduğu her işte çalışmaktı. Benzincide çalıştı, hademelik, garsonluk yaptı. Yaşananlar, kâğıda döküldüğünde bazen Çehov tadındaydı, bazen Kafka... İnsanların yaşamlarında barınan, gizlenen öyküleri, yalın, gerçekçi, acıtan şiirsel bir dille yansıttı. Yenilenler içkiye sığınırken, kısa öykü türünü yeniden var eden Carver, her başarısında içti, çok içti, ölümüne içti...</p>
+
+<p>Raymond Carver'ın son dönem öykülerini içeren Fil, yazarın en önemli eserlerinden biri olarak görülüyor.</p>"
+        
+    },
+    {
+        "itemName":"Küçük Kara Balık",
+        "itemAuthor":"Samed Behrengi",
+        "itemImage":"kapak/kucukkarabalik.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"
+Kış ortasında bir akşam vaktiydi. Denizin en derin yerinde, yaşlı mı yaşlı bir balık nine sayıları on iki bini bulan çocuklarıyla torunlarını çevresine toplamış, onlara bir masal anlatıyordu... "
+        
+        
+    },
+    {
+     "itemName":"Charlie'nin Çikolata Fabrikası",
+        "itemAuthor":"Roald Dahl",
+        "itemImage":"kapak/charlie.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription": "Charlie'nin çocukları heyecanlandıran, 
+büyükleri gülümseten öyküsü
+
+Charlie; annesi, babası, iki ninesi ve iki dedesiyle, 
+büyük bir kentin bitiminde, küçük bir tahta barakada yaşamaktadır.
+Yoksuldurlar. Charlie çikolataya bayılır, 
+ama alacak parası yoktur. Biriktirilen parayla, yılda bir kez,
+küçük bir çikolata girer evlerine. Bu büyük kentte, 
+Charlie'lerin evinden bile görülen, kocaman bir çikolata fabrikası vardır; dünyanın en ünlü çikolatalarını üretir. Günlerden bir gün, fabrikanın sahibi Bay Wonka, imparatorluğunu devredeceği bir varis seçmek için yarışma düzenlediğini açıklar; Charlie de adaylardan biridir..."
+    },
+     {
+         "itemName":"Uçan Sınıf",
+        "itemAuthor":"Erich Kastner",
+        "itemImage":"kapak/ucansinif.jpg",
+        "itemCreationDate":"2015",
+        "itemDescription":"Erich Kastner, çocuklar için de, büyükler için de yazan Almanya'nın ünlü yazarlarından biri. Uçan Sınıf'ın kahramanları, yatılı bir okulun çocukları. Yılbaşına az bir zaman kalmıştır. Çocuklar, Uçan Sınıf adında bir oyun hazırlamaya karar verirler. Ne var ki çalışmaları yarım kalır. Çünkü bir başka okulun öğrencileri, hem oyunda rol alan oyunculardan birini, hem de çocukların alıştırma kitaplarını kaçırmışlardır. Kastner'in 'Yazdığım en iyi kitap' dediği Uçan Sınıf, ince bir hüzün ve bolca mizahla kaleme alınmış bir başyapıt. Bu kitapta yazar, çocuklar arasındaki dayanışmanın, sıkı bir arkadaşlığın gücüyle yaşamın zorluklarını alt etmenin güzel bir örneğini veriyor."
+     } ];
+        
+        
